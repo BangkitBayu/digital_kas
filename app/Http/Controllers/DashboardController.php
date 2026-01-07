60c4f15cdd45d21601cf1c834c7252a44a11fdbb
BangkitBayu <martinusmartinussaja@gmail.com>
2026-01-07 19:39:27 +0700

    feat(dashboard.profile): make logic to handle edit profile

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class DashboardController extends Controller
 {
-    public function editProfile(Request $request) {
+    public function editProfile(Request $request)
+    {
+        $request->validate([
+            "kelas" => "required",
+            "jurusan" => "required",
+            "asal_sekolah" => "required",
+            "password_baru" => "required|min:8",
+            "konfirmasi_password" => "required|same:password_baru",
+            "password_lama" => "required"
+        ], [
+            'kelas.required' => "Kelas tidak boleh kosong.",
+            'jurusan.required' => "Jurusan tidak boleh kosong.",
+            'asal_sekolah.required' => "Asal sekolah tidak boleh kosong.",
+            'konfirmasi_password.required' => "Konfirmasi password tidak boleh kosong.",
+            'konfirmasi_password.same' => "Password tidak cocok.",
+            'password_baru.required' => "Password tidak boleh kosong.",
+            'password_baru.min' => "Password minimal 8 karakter.",
+            'password_lama.required' => "Password tidak boleh kosong.",
+        ]);
 
+        $payload = $request->only(["kelas", "jurusan", "asal_sekolah", "password_baru", "password_lama"]);
+
+        $classData = Kelas::where('kelas', $payload['kelas'])->first();
+
+        if (Hash::check($payload["password_lama"], $classData->password)) {
+            try {
+                $classData->password = Hash::make($payload["password_baru"]);
+                $classData->save();
+
+                return redirect()->back()->with("success", "Yay, profile akun kelas kamu berhasil diperbaharui.");
+            } catch (\Throwable $th) {
+                return redirect()->back()->with("error", "Maaf, terjadi kesalahan saat menyimpan perubahan. Silahkan coba lagi!");
+            }
+        } else {
+            return redirect()->back()->with("error", "Maaf, password yang kamu masukkan tidak cocok. Silahkan coba lagi!");
+        }
     }
 }
--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -17,6 +17,16 @@
                 <h2 class=" font-semibold text-xl text-primary">Profile {{ $data->kelas }}</h2>
                 <p class=" text-md text-secondary">Profile berisi seluruh detail akun kelas kamu.</p>
             </div>
+            @if (session('success'))
+                <div class=" border border-green-500 rounded-sm bg-green-100 text-green-500">
+                    <x-alert status="success" message="{{ session('success') }}"></x-alert>
+                </div>
+            @endif
+            @if (session('error'))
+                <div class=" border border-red-500 rounded-sm bg-red-100 text-red-500">
+                    <x-alert status="error" message="{{ session('error') }}"></x-alert>
+                </div>
+            @endif
             <div class="container border rounded-md border-[#d8d8d8da]">
                 <div class="container flex items-center space-x-2 border-b p-2 border-[#d8d8d8da]">
                     <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24">
@@ -33,21 +43,30 @@
                         <input type="text" value="{{ $data->kelas }}"
                             class=" w-full border border-[#d8d8d8da] px-3 py-2 rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary"
                             name="kelas">
+                        @error('kelas')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="form-group flex flex-col space-y-1">
                         <label for="kelas" class=" text-md text-primary">Jurusan</label>
                         <input type="text" value="{{ $data->jurusan }}"
                             class=" w-full border border-[#d8d8d8da] px-3 py-2 rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary"
                             name="jurusan">
+                        @error('jurusan')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="form-group flex flex-col space-y-1">
                         <label for="kelas" class=" text-md text-primary">Asal sekolah</label>
                         <input type="text" value="{{ $data->asal_sekolah }}"
                             class=" w-full border border-[#d8d8d8da] px-3 py-2 rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary"
                             name="asal_sekolah">
+                        @error('asal_sekolah')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="form-group w-full flex flex-col space-y-1">
-                        <label for="new password" class=" text-md text-primary">Password baru</label>
+                        <label for="password baru" class=" text-md text-primary">Password baru</label>
                         <div x-data="{ showPassword: false }"
                             class=" w-full border border-[#d8d8d8da] rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary flex">
                             <input x-bind:type="showPassword ? 'text' : 'password'" placeholder="Masukkan password baru"
@@ -73,69 +92,83 @@
                                 </svg>
                             </button>
                         </div>
-                        <div class="form-group w-full flex flex-col space-y-1">
-                            <label for="konfirmasi password" class=" text-md text-primary">Konfirmasi password
-                                baru</label>
-                            <div x-data="{ showPassword: false }"
-                                class=" w-full border border-[#d8d8d8da] rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary flex">
-                                <input x-bind:type="showPassword ? 'text' : 'password'"
-                                    placeholder="Konfirmasi password baru"
-                                    class="px-3 py-2 text-primary text-sm border-none outline-none w-full @error('password') border-red-500 @enderror"
-                                    name="konfirmasi_password">
-                                <button class=" bg-blue-500 px-3 py-2 rounded-r-md" id=" toggle-password" type="button"
-                                    @click = "showPassword = !showPassword">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24" id=" show-password" x-show="showPassword"
-                                        class=" transition-transform delay-150 ease-in-out">
-                                        <g fill="none" stroke="#ffffff" stroke-linecap="round"
-                                            stroke-linejoin="round" stroke-width="1.5">
-                                            <path d="M3 13c3.6-8 14.4-8 18 0" />
-                                            <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
-                                        </g>
-                                    </svg>
+                        @error('password_baru')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group w-full flex flex-col space-y-1">
+                        <label for="konfirmasi password" class=" text-md text-primary">Konfirmasi password
+                            baru</label>
+                        <div x-data="{ showPassword: false }"
+                            class=" w-full border border-[#d8d8d8da] rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary flex">
+                            <input x-bind:type="showPassword ? 'text' : 'password'"
+                                placeholder="Konfirmasi password baru"
+                                class="px-3 py-2 text-primary text-sm border-none outline-none w-full @error('password') border-red-500 @enderror"
+                                name="konfirmasi_password">
+                            <button class=" bg-blue-500 px-3 py-2 rounded-r-md" id=" toggle-password" type="button"
+                                @click = "showPassword = !showPassword">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    viewBox="0 0 24 24" id=" show-password" x-show="showPassword"
+                                    class=" transition-transform delay-150 ease-in-out">
+                                    <g fill="none" stroke="#ffffff" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="1.5">
+                                        <path d="M3 13c3.6-8 14.4-8 18 0" />
+                                        <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
+                                    </g>
+                                </svg>
 
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        x-show="!showPassword" viewBox="0 0 16 16" id=" hidden-password"
-                                        class=" transition-transform delay-150 ease-in-out">
-                                        <path fill="#ffffff"
-                                            d="m10.12 10.827l4.026 4.027a.5.5 0 0 0 .708-.708l-13-13a.5.5 0 1 0-.708.708l3.23 3.23A6 6 0 0 0 3.2 6.182a6.7 6.7 0 0 0-1.117 1.982c-.021.061-.047.145-.047.145l-.018.062s-.076.497.355.611a.5.5 0 0 0 .611-.355l.001-.003l.008-.025l.035-.109a5.7 5.7 0 0 1 .945-1.674a5 5 0 0 1 1.124-1.014L6.675 7.38a2.5 2.5 0 1 0 3.446 3.446m-.74-.74A1.5 1.5 0 1 1 7.413 8.12zM6.32 4.2l.854.854Q7.564 5 8 5c2.044 0 3.286.912 4.028 1.817a5.7 5.7 0 0 1 .945 1.674q.025.073.035.109l.008.025v.003l.001.001a.5.5 0 0 0 .966-.257v-.003l-.001-.004l-.004-.013a2 2 0 0 0-.06-.187a6.7 6.7 0 0 0-1.117-1.982C11.905 5.089 10.396 4 8.002 4c-.618 0-1.177.072-1.681.199" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div class="form-group w-full flex flex-col space-y-1">
-                                <label for="password" class=" text-md text-primary">Password</label>
-                                <p class=" text-secondary text-sm">Password saat ini diperlukan untuk melakukan
-                                    perubahan.</p>
-                                <div x-data="{ showPassword: false }"
-                                    class=" w-full border border-[#d8d8d8da] rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary flex">
-                                    <input x-bind:type="showPassword ? 'text' : 'password'" placeholder=""
-                                        class="px-3 py-2 text-primary text-sm border-none outline-none w-full @error('password') border-red-500 @enderror"
-                                        name="password">
-                                    <button class=" bg-blue-500 px-3 py-2 rounded-r-md" id=" toggle-password"
-                                        type="button" @click = "showPassword = !showPassword">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                            viewBox="0 0 24 24" id=" show-password" x-show="showPassword"
-                                            class=" transition-transform delay-150 ease-in-out">
-                                            <g fill="none" stroke="#ffffff" stroke-linecap="round"
-                                                stroke-linejoin="round" stroke-width="1.5">
-                                                <path d="M3 13c3.6-8 14.4-8 18 0" />
-                                                <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
-                                            </g>
-                                        </svg>
-
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                            x-show="!showPassword" viewBox="0 0 16 16" id=" hidden-password"
-                                            class=" transition-transform delay-150 ease-in-out">
-                                            <path fill="#ffffff"
-                                                d="m10.12 10.827l4.026 4.027a.5.5 0 0 0 .708-.708l-13-13a.5.5 0 1 0-.708.708l3.23 3.23A6 6 0 0 0 3.2 6.182a6.7 6.7 0 0 0-1.117 1.982c-.021.061-.047.145-.047.145l-.018.062s-.076.497.355.611a.5.5 0 0 0 .611-.355l.001-.003l.008-.025l.035-.109a5.7 5.7 0 0 1 .945-1.674a5 5 0 0 1 1.124-1.014L6.675 7.38a2.5 2.5 0 1 0 3.446 3.446m-.74-.74A1.5 1.5 0 1 1 7.413 8.12zM6.32 4.2l.854.854Q7.564 5 8 5c2.044 0 3.286.912 4.028 1.817a5.7 5.7 0 0 1 .945 1.674q.025.073.035.109l.008.025v.003l.001.001a.5.5 0 0 0 .966-.257v-.003l-.001-.004l-.004-.013a2 2 0 0 0-.06-.187a6.7 6.7 0 0 0-1.117-1.982C11.905 5.089 10.396 4 8.002 4c-.618 0-1.177.072-1.681.199" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    x-show="!showPassword" viewBox="0 0 16 16" id=" hidden-password"
+                                    class=" transition-transform delay-150 ease-in-out">
+                                    <path fill="#ffffff"
+                                        d="m10.12 10.827l4.026 4.027a.5.5 0 0 0 .708-.708l-13-13a.5.5 0 1 0-.708.708l3.23 3.23A6 6 0 0 0 3.2 6.182a6.7 6.7 0 0 0-1.117 1.982c-.021.061-.047.145-.047.145l-.018.062s-.076.497.355.611a.5.5 0 0 0 .611-.355l.001-.003l.008-.025l.035-.109a5.7 5.7 0 0 1 .945-1.674a5 5 0 0 1 1.124-1.014L6.675 7.38a2.5 2.5 0 1 0 3.446 3.446m-.74-.74A1.5 1.5 0 1 1 7.413 8.12zM6.32 4.2l.854.854Q7.564 5 8 5c2.044 0 3.286.912 4.028 1.817a5.7 5.7 0 0 1 .945 1.674q.025.073.035.109l.008.025v.003l.001.001a.5.5 0 0 0 .966-.257v-.003l-.001-.004l-.004-.013a2 2 0 0 0-.06-.187a6.7 6.7 0 0 0-1.117-1.982C11.905 5.089 10.396 4 8.002 4c-.618 0-1.177.072-1.681.199" />
+                                </svg>
+                            </button>
                         </div>
-                    </form>
-                    <button class=" text-white w-full px-3 py-2 rounded-md bg-blue-500 hover:bg-blue-400 transition-colors ease-in delay-150 mt-3" type="submit">Simpan perubahan</button>
+                        @error('konfirmasi_password')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group w-full flex flex-col space-y-1">
+                        <label for="password lama" class=" text-md text-primary">Password</label>
+                        <p class=" text-secondary text-sm">Password saat ini diperlukan untuk melakukan
+                            perubahan.</p>
+                        <div x-data="{ showPassword: false }"
+                            class=" w-full border border-[#d8d8d8da] rounded-md focus:outline-blue-500 ease-in delay-100 text-sm text-secondary flex">
+                            <input x-bind:type="showPassword ? 'text' : 'password'" placeholder=""
+                                class="px-3 py-2 text-primary text-sm border-none outline-none w-full @error('password') border-red-500 @enderror"
+                                name="password_lama">
+                            <button class=" bg-blue-500 px-3 py-2 rounded-r-md" id=" toggle-password" type="button"
+                                @click = "showPassword = !showPassword">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    viewBox="0 0 24 24" id=" show-password" x-show="showPassword"
+                                    class=" transition-transform delay-150 ease-in-out">
+                                    <g fill="none" stroke="#ffffff" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="1.5">
+                                        <path d="M3 13c3.6-8 14.4-8 18 0" />
+                                        <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
+                                    </g>
+                                </svg>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                    x-show="!showPassword" viewBox="0 0 16 16" id=" hidden-password"
+                                    class=" transition-transform delay-150 ease-in-out">
+                                    <path fill="#ffffff"
+                                        d="m10.12 10.827l4.026 4.027a.5.5 0 0 0 .708-.708l-13-13a.5.5 0 1 0-.708.708l3.23 3.23A6 6 0 0 0 3.2 6.182a6.7 6.7 0 0 0-1.117 1.982c-.021.061-.047.145-.047.145l-.018.062s-.076.497.355.611a.5.5 0 0 0 .611-.355l.001-.003l.008-.025l.035-.109a5.7 5.7 0 0 1 .945-1.674a5 5 0 0 1 1.124-1.014L6.675 7.38a2.5 2.5 0 1 0 3.446 3.446m-.74-.74A1.5 1.5 0 1 1 7.413 8.12zM6.32 4.2l.854.854Q7.564 5 8 5c2.044 0 3.286.912 4.028 1.817a5.7 5.7 0 0 1 .945 1.674q.025.073.035.109l.008.025v.003l.001.001a.5.5 0 0 0 .966-.257v-.003l-.001-.004l-.004-.013a2 2 0 0 0-.06-.187a6.7 6.7 0 0 0-1.117-1.982C11.905 5.089 10.396 4 8.002 4c-.618 0-1.177.072-1.681.199" />
+                                </svg>
+                            </button>
+                        </div>
+                        @error('password_lama')
+                            <span class=" text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                    </div>
             </div>
+            <button
+                class=" text-white w-full px-3 py-2 rounded-md bg-blue-500 hover:bg-blue-400 transition-colors ease-in delay-150 mt-3 cursor-pointer"
+                type="submit">Simpan
+                perubahan</button>
+            </form>
+        </div>
         </div>
     </main>
 </body>
